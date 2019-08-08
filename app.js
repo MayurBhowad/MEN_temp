ba5b58a9c594755719bdf68500635848b6477fbb
MayurBhowad <mayurbhowad1995@gmail.com>
2019-08-08 18:05:45 +0530

    db connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('dotenv/config');
+
 const app = express();
 
 //MIDDLEWARE
@@ -16,6 +18,6 @@ app.get('/posts', (req, res) => {
 });
 
 //DB connection
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, () => console.log('DB Connected..'));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log('DB Connected..'));
 
 app.listen(3000);
