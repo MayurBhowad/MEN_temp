266fcefdb26938aa5b3fcc537703c34d0d324734
MayurBhowad <mayurbhowad1995@gmail.com>
2019-08-08 18:02:21 +0530

    db connction

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//MIDDLEWARE
+
+
 //ROUTES
+app.get('/', (req, res) => {
+    res.send('app.js..app.get')
+});
+
+app.get('/posts', (req, res) => {
+    res.send('app.js..app.get../posts');
+});
+
+//DB connection
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, () => console.log('DB Connected..'));
+
+app.listen(3000);
